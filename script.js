22319e344bc289005f377556fff6f3c6232ed14d
Taha Kazmi <96362790+lfgraphics@users.noreply.github.com>
2023-08-01 14:18:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const dropArea = document.getElementById('drop-area');
+const inputFile = document.getElementById('input-file');
+const imageView = document.getElementById('imgview');
+
+inputFile.addEventListener('change', uploadImage);
+
+function uploadImage() {
+    const selectedFile = inputFile.files[0];
+
+    if (!selectedFile) {
+        console.error('No file selected.');
+        return;
+    }
+
+    if (!selectedFile.type.startsWith('image/')) {
+        console.error('Selected file is not an image.');
+        return;
+    }
+
+    let imgLink = URL.createObjectURL(selectedFile);
+    imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.innerHTML = ''
+    imageView.style.border = `none`;
+}
+
+dropArea.addEventListener('dragover', function(e){
+    e.preventDefault();
+});
+dropArea.addEventListener('drop', function(e){
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+});
